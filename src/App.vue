3bebbb550413e4bfcbc534b319a46fab0ca7171c
pros178178 <68476596+pros178178@users.noreply.github.com>
2023-06-29 13:59:09 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <StepFormValidation></StepFormValidation>
+  </div>
+</template>
+
+<script>
+import StepFormValidation from "./components/StepFormValidation.vue";
+
+export default {
+  name: "App",
+  components: {
+    StepFormValidation
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
